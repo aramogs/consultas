2197b9a9c16b38976d3864ad045d6067578d3fb8
aramogs <aramogs@users.noreply.github.com>
2020-01-02 10:05:09 -0700

    se limpia codigo

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
+$(document).ready(function () {
 
+  titulo = $('.csvTitulo').text()
+  $('#myTable').dataTable({
+      "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+      dom: 'Blfrtip',
+      buttons: [
+          {
+              extend: 'copyHtml5',
+              title: titulo
+          },
+          {
+              extend: 'csvHtml5',
+              title: titulo
+          }
+      ]
+  });
+});
 
 $("#menu-toggle").click(function (e) {
   e.preventDefault();
